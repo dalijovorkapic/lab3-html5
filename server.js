e0dab93cbe564d623a18165bbe12b6894139b93c
dalijovorkapic <dv52809@fer.hr>
2022-12-03 13:48:01 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const externalUrl = process.env.RENDER_EXTERNAL_URL;
+const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 4080;
+const hostname = '127.0.0.1';
+// Serve only the static files form the dist directory
+app.use(express.static('/'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('ind.html', {root: './'}),
+);
+
+app.listen(port, hostname);
+
